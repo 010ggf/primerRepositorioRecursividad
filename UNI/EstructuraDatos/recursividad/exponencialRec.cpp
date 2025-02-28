02d25960d8ad4c842449832cae37e2f31dbd4048
010ggf <score0125@gmail.com>
2025-02-28 10:33:18 -0600

    se agrego cambio en exponencialRec por nuevo metodo potencia

--- UNI/EstructuraDatos/recursividad/exponencialRec.cpp
+++ UNI/EstructuraDatos/recursividad/exponencialRec.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int potencia(int n, int p){
+    if (p==0) return 1;
+    return n*potencia(n, p-1);
+}
+
 int main(){
     int n, p;
     cout<<"dame un numero: ";
     cin>>n;
     cout<<"dame la potencia: "; 
     cin>>p;
+
+    potencia(n, p);
 
     return 0;
 }
